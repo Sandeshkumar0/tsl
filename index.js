f18096a1d917246439269db5c459b28ace7afdac
Sandesh kumar <157035433+Sandeshkumar0@users.noreply.github.com>
2025-08-22 21:11:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
     res.redirect('/api');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
